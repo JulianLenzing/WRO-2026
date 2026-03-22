d68fb8ddce19b38fcbca17600145ea6c0ce90f0b
JulianLenzing <julenzing43@saglh.de>
2026-03-22 14:51:01 +0100

    Applied local changes after recovering from corrupted repo

--- pi5Software/include/PwmController.h
+++ pi5Software/include/PwmController.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <algorithm>
 #include <PiPCA9685/PCA9685.h>
+#include <iostream>
 
 class PwmController {
 public:
@@ -18,9 +19,10 @@ public:
     virtual ~PwmController() {
         setMs(1.5);
     }
-    
+
 protected:
     void setMs(float ms){
+        ms = std::clamp(ms, 1.5f - dutyCycleRange / 2.0f, 1.5f + dutyCycleRange / 2.0f);
         int off = int(ms/20.0f*4096.0f);
         pca.set_pwm(line, 0, off);
     }
@@ -45,18 +47,60 @@ public:
 
 class ServoController : PwmController {
 public:
-    ServoController(int pLine, float pAngleRange, float pDutyCycleRange = 1.0f) 
-        : PwmController(pLine, pAngleRange), 
-          angleRange(pAngleRange)
+    ServoController(int pLine, float pAngleRange, float pDutyCycleRange = 1.0f) // Angles in radians
+        : PwmController(pLine, pDutyCycleRange), 
+          angleRange(pAngleRange),
+          minDutyCycle(1.5f - (pDutyCycleRange / 2.0f)),
+          maxDutyCycle(1.5f + (pDutyCycleRange / 2.0f)),
+          minAngle(2.0f*M_PI - (pAngleRange/2.0f)),
+          maxAngle(pAngleRange/2.0f)
     {
         setMs(1.5);
     }
 
-    void setAngle(float angle) {} // Implementation to be added
+    void setAngle(float angle) {
+        if(inverted) angle = 2.0f*M_PI - angle;
+        angle = normaliseAngle(angle);
+        if(angle <= M_PI && angle > maxAngle) {
+            angle = maxAngle;            
+        }
+        else if(angle > M_PI && angle < minAngle) {
+            angle = minAngle;
+        }
+        
+        if(angle <= M_PI) {
+            float ms = 1.5f - (angle / (angleRange/2.0f)) * (dutyCycleRange / 2.0f);
+            ms = std::clamp(ms, minDutyCycle, maxDutyCycle);
+            setMs(ms); 
+        }
+        else {
+            float inverseAngle = 2.0f*M_PI - angle;
+            float ms = 1.5f + (inverseAngle / (angleRange/2.0f)) * (dutyCycleRange / 2.0f);
+            ms = std::clamp(ms, minDutyCycle, maxDutyCycle);
+            setMs(ms);            
+        }
+    } 
+
     void setMiddle() { setAngle(0.0f); }
 
+    void invert(){
+        if(!inverted) inverted = true;
+        else inverted = false;
+    }
+
 protected:
+    float normaliseAngle(float angle) {
+        float ret = fmod(fmod(angle, 2.0f*M_PI) + 2.0f*M_PI, 2.0f*M_PI);
+        return ret;
+    }
+
+    bool inverted = false;
+
     float angleRange;
+    float minDutyCycle;
+    float maxDutyCycle;
+    float minAngle;
+    float maxAngle;
 };
 
   /*
